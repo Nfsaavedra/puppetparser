87e5ee65cfb0be376a10eb1f49b86d0aa08f3aaf
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-04-04 22:14:52 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='puppetparser',
+    version='0.1.0',
+    author='Nuno Saavedra',
+    author_email='nuno.saavedra',
+    packages=['puppetparser'],
+    description='A parser for Puppet',
+    install_requires=[
+    "ply"
+    ],
+)
