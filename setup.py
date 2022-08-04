ae0351d7ab3dcc8516947ff2c6174684d5cc12b8
Nfsaavedra <32967989+Nfsaavedra@users.noreply.github.com>
2022-08-04 17:32:13 +0100

    Change version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='puppetparser',
-    version='0.1.3',
+    version='0.1.4',
     author='Nuno Saavedra',
     author_email='nuno.saavedra@tecnico.ulisboa.pt',
     url="https://github.com/Nfsaavedra/puppetparser",
